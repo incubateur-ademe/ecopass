566ff7c5a0336711662c22bc8e171484750d8af7
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-04-25 09:33:09 +0200

    fix: remove missing function

--- src/scripts/getScores.tsx
+++ src/scripts/getScores.tsx
@@ -1,7 +1,6 @@
 import fs from "fs";
 import { getEcobalyseResults } from "../utils/ecobalyse/api";
 import { parseCSV } from "../utils/csv/parse";
-import { upsertProducts } from "../db/products";
 
 const getScores = async (file: string) => {
   const content = fs.readFileSync(file, { encoding: "utf-8" });
@@ -10,7 +9,6 @@ const getScores = async (file: string) => {
   const scores = await getEcobalyseResults(products);
 
   console.log(JSON.stringify(scores));
-  await upsertProducts(scores);
 };
 
 getScores(process.argv[2]);
