5afce63107954bd38a13c84598b7a080debc3eb3
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-05-15 17:56:59 +0200

    feat: proper label dimensions

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,15 +1,24 @@
+import classNames from "classnames"
 import { ProductWithScore } from "../../db/product"
 import styles from "./Label.module.css"
 
-const Label = ({ product: { score, standardized } }: { product: Exclude<ProductWithScore["score"], null> }) => {
+const Label = ({
+  product: { score, standardized },
+  secondary,
+}: {
+  product: Exclude<ProductWithScore["score"], null>
+  secondary?: boolean
+}) => {
   return (
-    <div className={styles.label}>
+    <div className={classNames(styles.label, { [styles.secondary]: secondary })}>
       <p className={styles.title}>
         Coût <b>environnemental</b>
       </p>
       <p className={styles.score}>{Math.round(score)}</p>
       <p className={styles.subtitle}>point d'impacts</p>
-      <p className={styles.impact}>{Math.round(standardized)} pts/100g</p>
+      <p className={classNames(styles.impact, { [styles.secondaryBackground]: secondary })}>
+        {Math.round(standardized)} pts/100g
+      </p>
     </div>
   )
 }
