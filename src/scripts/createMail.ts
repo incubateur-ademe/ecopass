894284b17b943b51235b74d4f30579dac999fbf0
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-05-22 17:36:18 +0200

    chore: add script to create user and brand

---
+++ src/scripts/createMail.ts
@@ -0,0 +1,43 @@
+import { prismaClient } from "../db/prismaClient"
+
+const email = ""
+const brandName = ""
+
+export const createMail = async () => {
+  if (!email || !brandName) {
+    console.log("Email or brand name is missing")
+    return
+  }
+
+  const user = await prismaClient.user.findUnique({
+    where: { email },
+  })
+  if (user) {
+    console.log("User alreay exists")
+    return
+  }
+
+  let brand = await prismaClient.brand.findFirst({
+    where: { name: brandName },
+  })
+
+  if (!brand) {
+    console.log("Brand not found, creating it...")
+    brand = await prismaClient.brand.create({
+      data: {
+        name: brandName,
+      },
+    })
+  }
+
+  await prismaClient.user.create({
+    data: {
+      email,
+      brandId: brand.id,
+      password: "",
+    },
+  })
+  console.log("User created")
+}
+
+createMail()
