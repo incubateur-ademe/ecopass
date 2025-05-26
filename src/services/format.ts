121d23eb83150a20d34410ab4d3a44282d522266
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-05-26 09:38:17 +0200

    fix: timezone issue

--- src/services/format.ts
+++ src/services/format.ts
@@ -1,5 +1,5 @@
-export const formatDateTime = (date: Date): string => date.toLocaleString("fr-FR")
-export const formatDate = (date: Date): string => date.toLocaleDateString("fr-FR")
+export const formatDateTime = (date: Date): string => date.toLocaleString("fr-FR", { timeZone: "Europe/Paris" })
+export const formatDate = (date: Date): string => date.toLocaleDateString("fr-FR", { timeZone: "Europe/Paris" })
 
 export const formatNumber = (number: number | undefined): string =>
   number ? Math.round(number).toLocaleString("fr-FR") : ""
