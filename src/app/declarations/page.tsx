3fbe8ec8be43d8201624a9289eed2731f38a2cd0
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-05-28 13:59:40 +0200

    fix: build with async searchparams

--- src/app/declarations/page.tsx
+++ src/app/declarations/page.tsx
@@ -1,8 +1,10 @@
 import { StartDsfrOnHydration } from "@codegouvfr/react-dsfr/next-app-router"
 import Declarations from "../../views/Declarations"
+import { PageProps } from "../../../.next/types/app/page"
 
-const DeclarationsPage = ({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined } }) => {
-  const page = searchParams?.page ? parseInt(searchParams.page as string, 10) : 1
+const DeclarationsPage = async ({ searchParams }: PageProps) => {
+  const params = await searchParams
+  const page = params.page ? parseInt(params.page as string, 10) : 1
   return (
     <>
       <StartDsfrOnHydration />
