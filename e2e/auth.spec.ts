85ac1192124146b414e659517c2789dc640b873f
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-06-25 09:04:47 +0200

    chore: use email/password in e2e

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -10,26 +10,18 @@ test("connection with proconnect, and properly disconnect", async ({ page }) => 
 
   await page.getByRole("textbox", { name: "Email professionnel Format" }).fill("ecopass-e2e@yopmail.com")
   await page.getByTestId("interaction-connection-button").click()
-  await page.getByRole("button", { name: "Recevoir un lien d’" }).click()
+  await page.getByRole("textbox", { name: "Renseignez votre mot de passe" }).fill("ecopasscestsupercool")
+  await page.getByRole("button", { name: "S’identifier" }).click()
 
-  await page.goto("https://yopmail.com/")
+  await expect(page.locator("#contenu").getByRole("heading", { name: "Déclarez le coût" })).toBeVisible()
+  await expect(page.locator("#contenu").getByRole("link", { name: "Se connecter" })).not.toBeVisible()
 
-  await page.getByRole("textbox", { name: "Login" }).fill("ecopass-e2e")
-  await page.getByRole("button", { name: "" }).click()
+  await page.getByRole("link", { name: "Se déconnecter" }).click()
 
-  const page1Promise = page.waitForEvent("popup")
-  await page.locator('iframe[name="ifmail"]').contentFrame().getByRole("link", { name: "Se connecter" }).click()
-  const page1 = await page1Promise
+  await expect(page.locator("#contenu").getByRole("heading", { name: "Déclarez le coût" })).toBeVisible()
+  await expect(page.locator("#contenu").getByRole("link", { name: "Se connecter" })).not.toBeVisible()
 
-  await expect(page1.locator("#contenu").getByRole("heading", { name: "Déclarez le coût" })).toBeVisible()
-  await expect(page1.locator("#contenu").getByRole("link", { name: "Se connecter" })).not.toBeVisible()
-
-  await page1.getByRole("link", { name: "Se déconnecter" }).click()
-
-  await expect(page1.locator("#contenu").getByRole("heading", { name: "Déclarez le coût" })).toBeVisible()
-  await expect(page1.locator("#contenu").getByRole("link", { name: "Se connecter" })).not.toBeVisible()
-
-  await page1.locator("#contenu").getByRole("link", { name: "Se connecter" }).click()
-  await page1.getByRole("button", { name: "S’identifier avec ProConnect" }).click()
-  await expect(page1.getByRole("textbox", { name: "Email professionnel Format" })).toBeVisible()
+  await page.locator("#contenu").getByRole("link", { name: "Se connecter" }).click()
+  await page.getByRole("button", { name: "S’identifier avec ProConnect" }).click()
+  await expect(page.getByRole("textbox", { name: "Email professionnel Format" })).toBeVisible()
 })
