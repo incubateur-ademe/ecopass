751f34244064990f4e709f157241143c17a884bf
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-09-12 14:39:27 +0200

    fix: auth on matomo api

--- src/services/stats/stats.ts
+++ src/services/stats/stats.ts
@@ -3,10 +3,18 @@ import { getProductCountByCategory } from "../../db/product"
 import { getDoneAPIUploadCount, getDoneFileUploadCount } from "../../db/upload"
 
 const getVisits = async () => {
+  const params = new URLSearchParams()
+  params.append("token_auth", process.env.MATOMO_API_TOKEN as string)
   return axios
-    .post<
-      [{ nb_visits: number }]
-    >(`${process.env.NEXT_PUBLIC_MATOMO_SITE_URL}?idSite=${process.env.NEXT_PUBLIC_MATOMO_SITE_ID}&method=VisitsSummary.get&format=JSON&module=API&period=year&date=today&showColumns=nb_visits&filter_limit=-1&flat=1`)
+    .post<[{ nb_visits: number }]>(
+      `${process.env.NEXT_PUBLIC_MATOMO_SITE_URL}?idSite=${process.env.NEXT_PUBLIC_MATOMO_SITE_ID}&method=VisitsSummary.get&format=JSON&module=API&period=year&date=today&showColumns=nb_visits&filter_limit=-1&flat=1`,
+      params,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+    )
     .then((response) => response.data)
 }
 
