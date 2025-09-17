bbc3ff22254e57158e103f07512956648c711c69
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-09-17 09:36:14 +0200

    chore: do not await middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,28 @@
 import { NextRequest, NextResponse } from "next/server"
 
-export async function middleware(request: NextRequest) {
-  try {
-    const { searchParams } = new URL(request.url)
-    const param = `e_c=API&e_a=${request.nextUrl.pathname}&e_n=${searchParams.toString()}`
+export function middleware(request: NextRequest) {
+  new Promise<void>(async (resolve, reject) => {
+    try {
+      const { searchParams } = new URL(request.url)
+      const param = `e_c=API&e_a=${request.nextUrl.pathname}&e_n=${searchParams.toString()}`
 
-    if (process.env.NEXT_PUBLIC_MATOMO === "true") {
+      if (process.env.NEXT_PUBLIC_MATOMO !== "true") {
+        console.log(`Fake matomo event: ${param}`)
+        return resolve()
+      }
+
       await fetch(
         `${process.env.NEXT_PUBLIC_MATOMO_SITE_URL}/matomo.php?idsite=${process.env.NEXT_PUBLIC_MATOMO_SITE_ID}&rec=1&${param}`,
         {
           method: "POST",
         },
       )
-    } else {
-      console.log(`Fake matomo event: ${param}`)
+      return resolve()
+    } catch (error) {
+      console.error("Erreur lors du tracking API:", error)
+      reject(error)
     }
-  } catch (error) {
-    console.error("Erreur lors du tracking API:", error)
-  }
+  })
 
   return NextResponse.next()
 }
