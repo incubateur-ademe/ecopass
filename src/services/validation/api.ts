5b636a900f0e9772d486b9dd7799c7d400be18b9
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-09-19 09:13:28 +0200

    feat: add test declaration on api

--- src/services/validation/api.ts
+++ src/services/validation/api.ts
@@ -30,6 +30,7 @@ const accessoryValidation = z.object({
 })
 
 const productAPIValidation = z.object({
+  test: z.boolean().optional(),
   gtins: z.array(z.string().regex(/^\d{8}$|^\d{13}$/, "Le code GTIN doit contenir 8 ou 13 chiffres")).min(1),
   internalReference: z.string(),
   declaredScore: z.number().optional(),
