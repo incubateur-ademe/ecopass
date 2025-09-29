b602301053fc0bee82f0ef76c16d674dcdc012e2
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-09-29 08:56:52 +0100

    fix: rounding on label

--- src/utils/label/size.ts
+++ src/utils/label/size.ts
@@ -1,14 +1,15 @@
 export const getSize = (value: number) => {
-  if (value < 10) {
+  const roundedValue = Math.round(value)
+  if (roundedValue < 10) {
     return 10
   }
-  if (value < 100) {
+  if (roundedValue < 100) {
     return 5
   }
-  if (value < 1000) {
+  if (roundedValue < 1000) {
     return -1
   }
-  if (value < 10000) {
+  if (roundedValue < 10000) {
     return -7
   }
   return -12
