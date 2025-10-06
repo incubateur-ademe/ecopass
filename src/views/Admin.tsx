4b667060b39a043999d12c4ea11f8091ebc98d1d
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-10-06 12:09:27 +0200

    feat: add organizations info (#49)

--- src/views/Admin.tsx
+++ src/views/Admin.tsx
@@ -2,61 +2,79 @@ import Block from "../components/Block/Block"
 import Table from "../components/Table/Table"
 import { AdminStats } from "../services/stats/stats"
 import { formatDateTime } from "../services/format"
+import { nafs } from "../utils/admin/nafs"
 
 const Admin = ({ stats }: { stats: AdminStats }) => {
   return (
     <Block>
-      <h1>Admin Dashboard</h1>
+      <h1>Statistiques admin</h1>
       {stats && (
-        <Table
-          fixed
-          caption='Statistiques'
-          noCaption
-          headers={[
-            "Nom",
-            "Utilisateurs",
-            "Premier upload",
-            "Dernier upload",
-            "Produits",
-            "Uploads fichier",
-            "Uploads API",
-            "Produits par marque",
-          ]}
-          data={stats
-            .sort((a, b) => {
-              if (b.uploadDates.last && a.uploadDates.last) {
-                return b.uploadDates.last.getTime() - a.uploadDates.last.getTime()
-              } else if (b.uploadDates.last) {
-                return 1
-              } else if (a.uploadDates.last) {
-                return -1
-              } else {
-                return a.organizationName.localeCompare(b.organizationName)
-              }
-            })
-            .map((info) => [
-              info.organizationName,
-              info.userCount,
-              info.uploadDates.first ? formatDateTime(info.uploadDates.first) : "",
-              info.uploadDates.last ? formatDateTime(info.uploadDates.last) : "",
-              info.totalProducts,
-              `${info.uploads.file} (${info.uploads.fileDone} réussi${info.uploads.fileDone > 1 ? "s" : ""})`,
-              info.uploads.api,
-              Object.keys(info.brandCounts).length > 0 ? (
-                <ul key={info.organizationName}>
-                  {Object.entries(info.brandCounts)
-                    .sort((a, b) => b[1] - a[1])
-                    .map(([name, count]) => (
-                      <li key={name}>
-                        {name} : {count}
-                      </li>
-                    ))}
-                </ul>
-              ) : (
-                ""
-              ),
-            ])}
-        />
+        <>
+          <p>
+            <b>{stats.reduce((acc, curr) => acc + curr.userCount, 0)}</b> utilisateurs dans <b>{stats.length}</b>{" "}
+            organisations
+          </p>
+          <Table
+            fixed
+            caption='Statistiques'
+            noCaption
+            headers={[
+              "Nom",
+              "Utilisateurs",
+              "Premier upload",
+              "Dernier upload",
+              "Produits",
+              "Uploads fichier",
+              "Uploads API",
+              "Produits par marque",
+            ]}
+            data={stats
+              .sort((a, b) => {
+                if (b.uploadDates.last && a.uploadDates.last) {
+                  return b.uploadDates.last.getTime() - a.uploadDates.last.getTime()
+                } else if (b.uploadDates.last) {
+                  return 1
+                } else if (a.uploadDates.last) {
+                  return -1
+                } else {
+                  return a.name.localeCompare(b.name)
+                }
+              })
+              .map((info) => [
+                <div key={info.name}>
+                  <span>
+                    {info.name} - {info.siret} ({info.effectif} salariés)
+                  </span>
+
+                  {info.naf !== null && (
+                    <>
+                      <br />
+                      {nafs[info.naf] || info.naf}
+                    </>
+                  )}
+                </div>,
+                info.userCount,
+                info.uploadDates.first ? formatDateTime(info.uploadDates.first) : "",
+                info.uploadDates.last ? formatDateTime(info.uploadDates.last) : "",
+                info.totalProducts,
+                `${info.uploads.file} (${info.uploads.fileDone} réussi${info.uploads.fileDone > 1 ? "s" : ""})`,
+                info.uploads.api,
+                Object.keys(info.brandCounts).length > 0 ? (
+                  <ul key={info.name}>
+                    {Object.entries(info.brandCounts)
+                      .sort((a, b) => b[1] - a[1])
+                      .map(([name, count]) => (
+                        <li key={name}>
+                          {name} : {count}
+                        </li>
+                      ))}
+                  </ul>
+                ) : (
+                  ""
+                ),
+              ])}
+          />
+        </>
       )}
     </Block>
   )
