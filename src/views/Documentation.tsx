ef7c0f967e1d0ccb1f962cf2b8cfd34532c29361
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-10-28 09:37:41 +0100

    feat: specify default values

--- src/views/Documentation.tsx
+++ src/views/Documentation.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Block from "../components/Block/Block"
 import AllDocs from "../components/Documentation/AllDocs"
+import Default from "../components/Documentation/Default"
 
 const Documentation = () => (
   <>
@@ -15,15 +16,28 @@ const Documentation = () => (
         <Link href='/exemple/exemple.xlsx' className='fr-link' prefetch={false}>
           télécharger notre template
         </Link>
-        , qui contient une liste des valeurs autorisées
+        , qui contient une liste des valeurs autorisées.
       </p>
       <p>
         Pour générer votre fichier CSV, vous pouvez utiliser des valeurs en français, anglais, ou si vous utilisez déjà
         l'API Ecobalyse, les valeurs de cette dernière.
       </p>
+      <p>
+        Lorsqu'une valeur optionnelle n'est pas fournie, la valeur par défaut utilisée dépend de la catégorie du
+        produit. Vous pouvez retrouver ces valeurs dans{" "}
+        <Link href='#default' className='fr-link'>
+          ce tableau
+        </Link>
+        .
+      </p>
     </Block>
-    <Block noMargin>
+    <Block>
+      <h2>Format du fichier de déclaration</h2>
       <AllDocs />
+    </Block>
+    <Block>
+      <h2>Valeurs par défaut</h2>
+      <Default />
     </Block>
   </>
 )
