b12f8ea197b24c945babdf1f779a1c91607b9cfe
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-11-11 09:43:45 +0100

    chore: update npm packages

--- src/app/logout/proconnect/page.tsx
+++ src/app/logout/proconnect/page.tsx
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from "uuid"
 import { redirect } from "next/navigation"
-import { PageProps } from "../../../../.next/types/app/page"
+import { PageProps } from "../../../types/Next"
 
 const Logout = async ({ searchParams }: PageProps) => {
   const logOutUrl = new URL(`${process.env.PROCONNECT_DOMAIN}/api/v2/session/end`)
-  logOutUrl.searchParams.set("id_token_hint", (await searchParams).id_token_hint || "")
+  const params = await searchParams
+  const idTokenHint = Array.isArray(params.id_token_hint) ? params.id_token_hint[0] : params.id_token_hint
+  logOutUrl.searchParams.set("id_token_hint", idTokenHint || "")
   logOutUrl.searchParams.set("post_logout_redirect_uri", process.env.NEXTAUTH_URL!)
   logOutUrl.searchParams.set("state", uuidv4())
   redirect(logOutUrl.toString())
