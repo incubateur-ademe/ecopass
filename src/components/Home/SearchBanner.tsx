fa2c423096165a5298c8ebe212271f727c78a29e
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-11-18 16:13:35 +0100

    fix: typo in search

--- src/components/Home/SearchBanner.tsx
+++ src/components/Home/SearchBanner.tsx
@@ -8,7 +8,7 @@ const SearchBanner = () => {
       <p className={styles.description}>
         Vous êtes un consommateur et souhaitez en savoir plus sur les produits textiles que vous achetez ?
         <br />
-        Vous pouvez rechercher un produit par son code-barres pour avoir plus d'informations sur sont coût
+        Vous pouvez rechercher un produit par son code-barres pour avoir plus d'informations sur son coût
         environnemental.
       </p>
       <Search withAlert />
