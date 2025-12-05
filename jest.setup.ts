ec967701b74da74b57cc310170b02a5de38eb595
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-12-05 11:34:27 +0100

    chore: update prisma version

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,15 +1,13 @@
 import "dotenv/config"
 import { PrismaClient } from "./prisma/src/prisma"
+import { PrismaPg } from "@prisma/adapter-pg"
 
-const prismaTest = new PrismaClient({
-  datasources: {
-    db: {
-      url:
-        process.env.CI === "true"
-          ? process.env.DATABASE_URL
-          : "postgresql://ecopass_test:ecopass_test@localhost:5433/ecopass_test",
-    },
-  },
+const adapter = new PrismaPg({
+  connectionString:
+    process.env.CI === "true"
+      ? process.env.DATABASE_URL
+      : "postgresql://ecopass_test:ecopass_test@localhost:5433/ecopass_test",
 })
+const prismaTest = new PrismaClient({ adapter })
 
 export { prismaTest }
