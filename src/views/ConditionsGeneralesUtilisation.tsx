fec612a38640df0c1f08c3274848140651121289
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-12-29 10:41:34 +0100

    feat: add CGU

---
+++ src/views/ConditionsGeneralesUtilisation.tsx
@@ -0,0 +1,175 @@
+import Block from "../components/Block/Block"
+import styles from "./MentionsLegales.module.css"
+
+const ConditionsGeneralesUtilisation = () => {
+  return (
+    <Block className={styles.container}>
+      <h1>Conditions générales d’utilisation</h1>
+      <p>Mis à jour le 18/11/2025</p>
+      <br />
+      <p>
+        Les présentes conditions générales d’utilisation (dites « CGU ») fixent le cadre juridique du téléservice lié à
+        l’information du coût environnemental dénommé « Affichage du coût environnemental » et définissent les
+        conditions d’accès et d’utilisation des services par l’Utilisateur.
+      </p>
+
+      <h2>Article 1 - Champ d’application</h2>
+      <p>
+        Le présent site est ouvert et accessible à toute personne qui souhaite s’informer sur le coût environnemental
+        des produits. L’accès au téléservice lié à la déclaration de ces informations est ouvert aux seuls déclarants au
+        sens des présentes conditions générales d’utilisation.
+      </p>
+
+      <h2>Article 2 – Objet</h2>
+      <p>
+        Le site et le téléservice ont pour objet d’informer les personnes sur le coût environnemental et le détail des
+        impacts environnementaux d’une référence de produit textile, conformément au décret et à l’arrêté relatifs à
+        l’affichage du coût environnemental.
+      </p>
+
+      <h2>Article 3 – Définitions</h2>
+      <ul>
+        <li>
+          « DGCCRF » est la Direction générale de la Concurrence, de la Consommation et de la Répression des fraudes.
+        </li>
+        <li>
+          « Contrôleur » est tout compte relevant des agents habilités au titre de l’article L.511-7 du code de la
+          consommation et aux agents des ministères chargés de l’environnement et de l’économie, ou de l’agence de
+          l’environnement et de la maîtrise de l’énergie.
+        </li>
+        <li>
+          « Produit textile » : tout produit réalisé par un fabricant et mis sur le marché sur le territoire français.
+        </li>
+        <li>
+          « Fabricant » : toute personne physique ou morale qui fabrique un produit ou le fait concevoir et le
+          commercialise sous son nom ou pour sa marque, et le cas échéant tenue de déclarer le coût environnemental de
+          ses produits.
+        </li>
+        <li>
+          « Déclarant » : toute personne qui, pour elle-même ou pour le compte d’un fabricant, d’un importateur ou d’un
+          metteur sur le marché, effectue la déclaration relative au coût environnemental de ses produits ou des
+          produits de l’entité qu’elle représente.
+        </li>
+        <li>
+          « Coût environnemental » : l’information relative aux impacts environnementaux d’un produit, telle que
+          mentionnée à l’article L. 541-9-11. Elle consiste en un nombre entier supérieur à zéro, et s’exprime en points
+          d’impact. Le coût environnemental est le résultat de l’agrégation des différentes catégories d’impacts
+          environnementaux d’un produit tout au long de son cycle de vie, qui comprend notamment les étapes de
+          production des matières premières, transformation, distribution, d’utilisation et de fin de vie.
+        </li>
+        <li>« Mise sur le marché » : la première mise à disposition d’un produit sur le marché national.</li>
+        <li>« Utilisateur » : toute personne navigante sur le site, dont les déclarants.</li>
+        <li>Les « Services » sont les fonctionnalités offertes par le téléservice pour répondre à ses finalités.</li>
+      </ul>
+
+      <h2>Article 4 – Fonctionnalités</h2>
+      <h3>4.1 Création de compte</h3>
+      <p>
+        La création de compte est ouverte aux seuls déclarants. Pour ce faire ils doivent s’identifier via « ProConnect
+        ». Ils remplissent dès lors leur e-mail professionnel et leur mot de passe ou, le cas échéant, ils reçoivent un
+        lien d’identification leur permettant d’accéder au compte. Les déclarants doivent créer un compte pour la seule
+        déclaration du coût environnemental.
+      </p>
+      <h3>4.2 Fonctionnalités ouvertes aux déclarants</h3>
+      <h4>A – Déclarer les produits et leur coût environnemental</h4>
+      <p>
+        Les déclarants ont accès à la fonctionnalité leur permettant de déclarer les produits qu’ils commercialisent ou
+        pour lesquels ils sont autorisés à réaliser la déclaration. Ils ont une visibilité sur l’ensemble de leurs
+        produits et sur les fiches publiques.
+      </p>
+      <br />
+      <h4>B – Transmettre les informations aux autorités compétentes</h4>
+      <p>Les informations confidentielles sont transmises, le cas échéant, aux autorités de contrôle compétentes.</p>
+      <h3>4.3 Fonctionnalités ouvertes aux comptes « Contrôleur »</h3>
+      <h4>A – Fonctionnalités communes aux comptes « Contrôleur »</h4>
+      <p>
+        Dans le cadre de son pouvoir de contrôle et de sanction du respect par les personnes physiques et morales, de
+        l’affichage relevant de l’article L.541-9-11 du code de l’environnement, un compte « Contrôleur » peut être
+        créé. Le compte Contrôleur pourra :
+      </p>
+      <ul>
+        <li>
+          Recevoir les informations publiques et celles relatives aux calculs des coûts environnementaux des personnes
+          physiques et morales déclarantes ;
+        </li>
+        <li>
+          Calculer les coûts environnementaux des biens et services déclarés par les personnes physiques et morales pour
+          prévenir les déclarants en cas d’erreur ;
+        </li>
+        <li>
+          Vérifier que les produits entrant dans le champ du règlement (UE) n°1007/2011 et soumis à l’obligation
+          d’affichage du coût environnemental sont correctement identifiés. Notamment, il vérifie que le produit est
+          rattaché au bon fabricant, ou le cas échéant à l’importateur chargé de la mise sur le marché du produit
+          contrôlé.
+        </li>
+      </ul>
+      <p>
+        En cas de manquement aux obligations d’affichage prévues, une procédure de sanction pourra être engagée par
+        l’administration en dehors de la présente plateforme.
+      </p>
+      <br />
+      <h4>B – Fonctionnalités spécifiques à certains comptes « Contrôleur »</h4>
+      <p>
+        Les agents de la DGCCRF disposent de toutes les fonctionnalités ouvertes aux comptes « Contrôleur ». Ils peuvent
+        également, sur le fondement des vérifications et calculs effectués, sanctionner les fabricants ou importateurs
+        chargés de la mise sur le marché.
+      </p>
+      <h3>4.4 Autres fonctionnalités</h3>
+      <p>
+        Tout utilisateur peut consulter, rechercher ou télécharger les informations publiques relatives aux produits
+        soumis à l’obligation d’information prévue par la loi portant lutte contre le dérèglement climatique et
+        renforcement de la résilience face à ses effets. La consultation et la recherche se font via une barre de
+        recherche simple. Le téléchargement est fait dans un format compréhensible et réutilisable conformément aux
+        exigences du code des relations entre le public et l’administration. La réutilisation de ces données est soumise
+        aux dispositions prévues dans le code des relations entre le public et l’administration.
+      </p>
+
+      <h2>Article 5 – Responsabilité de l’éditeur</h2>
+      <p>
+        Les sources des informations diffusées sur le site sont réputées fiables mais le site ne garantit pas qu’il soit
+        exempt de défauts, d’erreurs ou d’omissions.
+      </p>
+      <p>
+        L’éditeur s’engage à la sécurisation du site, notamment en prenant toutes les mesures nécessaires permettant de
+        garantir la sécurité et la confidentialité des informations fournies. Il le réalise dans la limite des capacités
+        techniques et des moyens dont il dispose.
+      </p>
+      <p>
+        L’éditeur fournit les moyens nécessaires et raisonnables pour assurer un accès continu au site. Il se réserve la
+        liberté de faire évoluer, de modifier ou de suspendre, sans préavis, le site pour des raisons de maintenance ou
+        pour tout autre motif jugé nécessaire.
+      </p>
+
+      <h2>Article 6 – Responsabilité du déclarant</h2>
+      <p>
+        Toute information transmise par le déclarant est de sa seule responsabilité. Il est rappelé que toute personne
+        procédant à une fausse déclaration pour elle-même ou pour autrui s’expose, notamment, aux sanctions prévues à
+        l’article 441-1 du code pénal, prévoyant des peines pouvant aller jusqu’à trois ans d’emprisonnement et 45 000
+        euros d’amende. Le déclarant assure être la personne habilitée à réaliser les déclarations et publicité des
+        informations relatives au coût environnemental par voie dématérialisée. L’éditeur est fondé à engager sa
+        responsabilité sur ce fondement en cas de litige de son administration ou avec un tiers.
+      </p>
+      <p>
+        Toute usurpation d’identité entraîne la suppression immédiate du compte, et le cas échéant des poursuites
+        pénales. Lorsqu’un profil déclarant fait l’objet d’une telle usurpation, le compte est suspendu par l’éditeur.
+        Celui-ci prend contact avec l’organisation et effectue les vérifications nécessaires.
+      </p>
+      <p>
+        Le déclarant s’engage à ne pas mettre en ligne de contenus ou informations contraires aux dispositions légales
+        et réglementaires en vigueur. En particulier, tout commentaire, propos ou message, à quelque endroit que ce soit
+        au sein du présent téléservice comportant des éléments mensongers, racistes, sexistes, injurieux ou insultants
+        est interdit et pourra entraîner la suspension du compte par l’éditeur.
+      </p>
+
+      <h2>Article 7 – Mise à jour des conditions d’utilisation</h2>
+      <p>
+        Conformément à l’article L.112-9 du code des relations entre le public et l’administration, les termes des
+        présentes conditions d’utilisation de ce téléservice peuvent être amendés à tout moment, sans préavis, en
+        fonction des modifications apportées à la plateforme, de l’évolution de la législation ou pour tout autre motif
+        jugé nécessaire.
+      </p>
+    </Block>
+  )
+}
+
+export default ConditionsGeneralesUtilisation
