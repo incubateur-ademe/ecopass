63c90e5d2bdd8bf6565515652e36c5a915004c5c
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-01-16 12:42:23 +0100

    fix: paginate brand product (#93)

--- src/app/marques/[brandId]/page.tsx
+++ src/app/marques/[brandId]/page.tsx
@@ -3,33 +3,38 @@ import { notFound } from "next/navigation"
 import { getBrandWithProducts } from "../../../db/brands"
 import BrandDetail from "../../../views/BrandDetail"
 import { Metadata } from "next"
+import { getPublicProductsByBrandId } from "../../../db/product"
 
 type Props = {
   params: Promise<{ brandId: string }>
+  searchParams: Promise<{ page?: string }>
 }
 
 export const metadata: Metadata = {
   title: "Marques - Affichage environnemental",
 }
 
-const BrandPage = async ({ params }: Props) => {
+const BrandPage = async ({ params, searchParams }: Props) => {
   console.log("[MEMORY][marques/[brandId]/page][start]", process.memoryUsage())
   const { brandId } = await params
+  const { page } = await searchParams
+  const currentPage = parseInt(page || "1", 10)
+
   const brandData = await getBrandWithProducts(brandId)
-  let result
   if (!brandData) {
     console.log("[MEMORY][marques/[brandId]/page][end]", process.memoryUsage())
-    notFound()
-  } else {
-    result = (
-      <>
-        <StartDsfrOnHydration />
-        {brandData && <BrandDetail brand={brandData.brand} products={brandData.products} />}
-      </>
-    )
-    console.log("[MEMORY][marques/[brandId]/page][end]", process.memoryUsage())
-    return result
+    return notFound()
   }
+
+  const products = await getPublicProductsByBrandId(brandId, currentPage)
+  const result = (
+    <>
+      <StartDsfrOnHydration />
+      <BrandDetail brand={brandData} products={products} currentPage={currentPage} />
+    </>
+  )
+  console.log("[MEMORY][marques/[brandId]/page][end]", process.memoryUsage())
+  return result
 }
 
 export default BrandPage
--- src/views/BrandDetail.tsx
+++ src/views/BrandDetail.tsx
@@ -3,12 +3,20 @@ import Block from "../components/Block/Block"
 import BrandHeader from "../components/Brand/BrandHeader"
 import BrandProductsTable from "../components/Brand/BrandProductsTable"
 import { Products } from "../db/product"
-import { computeCategories, getLastDeclarationDate } from "../utils/brands/computeBrandStats"
+import { getLastDeclarationDate } from "../utils/brands/computeBrandStats"
+import { BrandInformation } from "../db/brands"
 
-const BrandDetail = ({ brand, products }: { brand: { id: string; name: string }; products: Products }) => {
-  const productCount = products.length
+const BrandDetail = ({
+  brand,
+  products,
+  currentPage,
+}: {
+  brand: BrandInformation
+  products: Products
+  currentPage: number
+}) => {
+  const productCount = brand.productsByCategory.reduce((acc, current) => acc + current.count, 0)
   const lastDeclarationDate = getLastDeclarationDate(products)
-  const categories = computeCategories(products)
 
   if (productCount === 0) {
     return (
@@ -26,12 +34,17 @@ const BrandDetail = ({ brand, products }: { brand: { id: string; name: string };
           name={brand.name}
           lastDeclarationDate={lastDeclarationDate}
           productCount={productCount}
-          categories={categories}
+          categories={brand.productsByCategory}
         />
       </Block>
 
       <Block>
-        <BrandProductsTable products={products} />
+        <BrandProductsTable
+          products={products}
+          currentPage={currentPage}
+          brandId={brand.id}
+          totalPages={Math.ceil(productCount / 10)}
+        />
       </Block>
     </>
   )
