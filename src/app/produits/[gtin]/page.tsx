09f2f040288d295896187e7f9afe633c0ef1201d
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-01-16 14:36:05 +0100

    fix: missing metadata

--- src/app/produits/[gtin]/page.tsx
+++ src/app/produits/[gtin]/page.tsx
@@ -1,16 +1,27 @@
-import { getProductWithScore } from "../../../db/product"
+import { getProductByGtin, getProductWithScore } from "../../../db/product"
 import Product from "../../../views/Product"
 import { StartDsfrOnHydration } from "@codegouvfr/react-dsfr/next-app-router"
 import EmptyProduct from "../../../views/EmptyProduct"
 import { Metadata } from "next"
 import { tryAndGetSession } from "../../../services/auth/redirect"
 
-export const metadata: Metadata = {
-  title: "Produit - Affichage environnemental",
-}
-
 type Props = {
   params: Promise<{ gtin: string }>
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { gtin } = await params
+  const product = await getProductByGtin(gtin)
+
+  if (!product) {
+    return {
+      title: "Produit - Affichage environnemental",
+    }
+  }
+
+  return {
+    title: `${product.internalReference} - Affichage environnemental`,
+  }
 }
 
 const ProductPage = async (props: Props) => {
