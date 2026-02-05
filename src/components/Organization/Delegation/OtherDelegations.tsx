ecaaaea68643080f1679f9ad9481c67ec54af239
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-02-05 17:08:43 +0100

    fix: unique id text

--- src/components/Organization/Delegation/OtherDelegations.tsx
+++ src/components/Organization/Delegation/OtherDelegations.tsx
@@ -1,21 +1,16 @@
 import Alert from "@codegouvfr/react-dsfr/Alert"
 import { UserOrganization } from "../../../db/user"
 import Delegations from "./Delegations"
+import Badge from "@codegouvfr/react-dsfr/Badge"
 
 const OtherDelegations = ({ organization }: { organization: UserOrganization }) => {
   return (
     <>
       {!organization.siret && organization.uniqueId && (
-        <Alert
-          severity='info'
-          small
-          description={
-            <>
-              Pour être référencé en tant que délégataire par une marque, veuillez leur communiquer votre identifiant
-              unique : <b>{organization.uniqueId}</b>
-            </>
-          }
-        />
+        <p className='fr-mb-2w'>
+          Pour être référencé en tant que délégataire par une marque, veuillez leur communiquer votre identifiant
+          unique : <Badge>{organization.uniqueId}</Badge>
+        </p>
       )}
       {organization.authorizedBy.length === 0 ? (
         <Alert severity='info' small description='Aucune entreprise ne vous a délégué de droits pour le moment.' />
