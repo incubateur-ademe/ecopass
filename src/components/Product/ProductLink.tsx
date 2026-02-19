67028cd32cc2e3148b56789c19bc72100c6895f7
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-02-19 15:06:43 +0100

    fix: link product version

--- src/components/Product/ProductLink.tsx
+++ src/components/Product/ProductLink.tsx
@@ -5,15 +5,7 @@ import styles from "./ProductLink.module.css"
 import { Products } from "../../db/product"
 import { useSearchParams } from "next/navigation"
 
-const ProductLink = ({
-  product,
-  version,
-  brandId,
-}: {
-  product: Products[number]
-  version?: string
-  brandId?: string
-}) => {
+const ProductLink = ({ product, brandId }: { product: Products[number]; brandId?: string }) => {
   const params = useSearchParams()
   return (
     <Button
@@ -21,8 +13,8 @@ const ProductLink = ({
       size='small'
       linkProps={{
         href: brandId
-          ? `/marques/${brandId}/${product.gtins[0]}${version ? `/${version}` : ""}?${params.toString()}`
-          : `/produits/${product.gtins[0]}${version ? `/${version}` : ""}?${params.toString()}`,
+          ? `/marques/${brandId}/${product.gtins[0]}/${product.id}?${params.toString()}`
+          : `/produits/${product.gtins[0]}/${product.id}?${params.toString()}`,
       }}
       key={product.id}
       className={styles.displayButton}>
