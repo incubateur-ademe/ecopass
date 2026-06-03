44a4ada4c80f25a5ba737ae3a563b5775e028611
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-06-03 13:28:34 +0200

    fix: only display last active link

--- src/components/Organization/Links.tsx
+++ src/components/Organization/Links.tsx
@@ -12,32 +12,61 @@ const Links = ({ organization }: { organization: Organization }) => {
     ...organization.authorizedBy.flatMap((authorization) => authorization.from.brands),
     ...organization.brands,
   ]
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .filter((brand, index, allBrands) => allBrands.findIndex((otherBrand) => otherBrand.id === brand.id) === index)
 
+  const consultancies = organization.authorizedOrganizations
+    .sort((a, b) => a.to.name.localeCompare(b.to.name))
+    .filter((link, index, allLinks) => {
+      if (!link.active) {
+        if (allLinks.some((otherLink) => otherLink.to.id === link.to.id && otherLink.active)) {
+          return false
+        }
+
+        if (allLinks.findIndex((otherLink) => otherLink.to.id === link.to.id && !otherLink.active) !== index) {
+          return false
+        }
+      }
+      return true
+    })
+
+  const organizations = organization.authorizedBy
+    .sort((a, b) => a.from.name.localeCompare(b.from.name))
+    .filter((link, index, allLinks) => {
+      if (!link.active) {
+        if (allLinks.some((otherLink) => otherLink.from.id === link.from.id && otherLink.active)) {
+          return false
+        }
+
+        if (allLinks.findIndex((otherLink) => otherLink.from.id === link.from.id && !otherLink.active) !== index) {
+          return false
+        }
+      }
+      return true
+    })
   return (
     <>
       {organization.type !== OrganizationType.Consultancy && organization.authorizedOrganizations.length > 0 && (
         <div data-testid='consultancies-organizations-list'>
           <Table
             fixed
-            caption={`${organization.authorizedOrganizations.length} Bureau${organization.authorizedOrganizations.length > 1 ? "x" : ""} d’études lié${organization.authorizedOrganizations.length > 1 ? "s" : ""}`}
+            caption={`${consultancies.length} Bureau${consultancies.length > 1 ? "x" : ""} d’études lié${consultancies.length > 1 ? "s" : ""}`}
             headers={["Nom", "Nom d'usage", "SIRET", "Etat", "Références", "Dernière date de dépôt", "Détails"]}
-            data={organization.authorizedOrganizations
-              .sort((a, b) => a.to.name.localeCompare(b.to.name))
-              .map((link) => [
-                <b key={link.to.id}>{link.to.name}</b>,
-                link.to.displayName,
-                link.to.siret || "N/A",
-                <Badge key={link.to.id} severity={link.active ? "success" : "error"} noIcon>
-                  {link.active ? "Actif" : "Inactif"}
-                </Badge>,
-                <Badge key={link.to.id} severity='info' noIcon>
-                  {link.references}
-                </Badge>,
-                link.lastDeclaration ? formatDate(link.lastDeclaration) : "",
-                <Button key={link.to.id} priority='secondary' linkProps={{ href: `/organisations/${link.to.id}` }}>
-                  Voir le détail
-                </Button>,
-              ])}
+            data={consultancies.map((link) => [
+              <b key={link.to.id}>{link.to.name}</b>,
+              link.to.displayName,
+              link.to.siret || "N/A",
+              <Badge key={link.to.id} severity={link.active ? "success" : "error"} noIcon>
+                {link.active ? "Actif" : "Inactif"}
+              </Badge>,
+              <Badge key={link.to.id} severity='info' noIcon>
+                {link.references}
+              </Badge>,
+              link.lastDeclaration ? formatDate(link.lastDeclaration) : "",
+              <Button key={link.to.id} priority='secondary' linkProps={{ href: `/organisations/${link.to.id}` }}>
+                Voir le détail
+              </Button>,
+            ])}
           />
         </div>
       )}
@@ -45,25 +74,23 @@ const Links = ({ organization }: { organization: Organization }) => {
         <div data-testid='brands-organizations-list'>
           <Table
             fixed
-            caption={`${organization.authorizedBy.length} Marque${organization.authorizedBy.length > 1 ? "s" : ""}`}
+            caption={`${organizations.length} Marque${organizations.length > 1 ? "s" : ""}`}
             headers={["Nom", "Nom d'usage", "SIRET", "Etat", "Références", "Dernière date de dépôt", "Détails"]}
-            data={organization.authorizedBy
-              .sort((a, b) => a.from.name.localeCompare(b.from.name))
-              .map((link) => [
-                <b key={link.from.id}>{link.from.name}</b>,
-                link.from.displayName,
-                link.from.siret || "N/A",
-                <Badge key={link.from.id} severity={link.active ? "success" : "error"} noIcon>
-                  {link.active ? "Active" : "Inactive"}
-                </Badge>,
-                <Badge key={link.from.id} severity='info' noIcon>
-                  {link.references}
-                </Badge>,
-                link.lastDeclaration ? formatDate(link.lastDeclaration) : "",
-                <Button key={link.from.id} priority='secondary' linkProps={{ href: `/organisations/${link.from.id}` }}>
-                  Voir le détail
-                </Button>,
-              ])}
+            data={organizations.map((link) => [
+              <b key={link.from.id}>{link.from.name}</b>,
+              link.from.displayName,
+              link.from.siret || "N/A",
+              <Badge key={link.from.id} severity={link.active ? "success" : "error"} noIcon>
+                {link.active ? "Active" : "Inactive"}
+              </Badge>,
+              <Badge key={link.from.id} severity='info' noIcon>
+                {link.references}
+              </Badge>,
+              link.lastDeclaration ? formatDate(link.lastDeclaration) : "",
+              <Button key={link.from.id} priority='secondary' linkProps={{ href: `/organisations/${link.from.id}` }}>
+                Voir le détail
+              </Button>,
+            ])}
           />
         </div>
       )}
@@ -83,31 +110,29 @@ const Links = ({ organization }: { organization: Organization }) => {
               </tr>
             </thead>
             <tbody>
-              {brands
-                .sort((a, b) => a.name.localeCompare(b.name))
-                .map((link) => (
-                  <tr key={link.id}>
-                    <td colSpan={3}>
-                      <b>{link.name}</b>
-                    </td>
-                    <td>
-                      <Badge severity={link.active ? "success" : "error"} noIcon>
-                        {link.active ? "Active" : "Inactive"}
-                      </Badge>
-                    </td>
-                    <td>
-                      <Badge severity='info' noIcon>
-                        {link.references}
-                      </Badge>
-                    </td>
-                    <td>{link.lastDeclaration ? formatDate(link.lastDeclaration) : ""}</td>
-                    <td>
-                      <Button priority='secondary' linkProps={{ href: `/marques/${link.id}` }}>
-                        Voir le détail
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
+              {brands.map((link) => (
+                <tr key={link.id}>
+                  <td colSpan={3}>
+                    <b>{link.name}</b>
+                  </td>
+                  <td>
+                    <Badge severity={link.active ? "success" : "error"} noIcon>
+                      {link.active ? "Active" : "Inactive"}
+                    </Badge>
+                  </td>
+                  <td>
+                    <Badge severity='info' noIcon>
+                      {link.references}
+                    </Badge>
+                  </td>
+                  <td>{link.lastDeclaration ? formatDate(link.lastDeclaration) : ""}</td>
+                  <td>
+                    <Button priority='secondary' linkProps={{ href: `/marques/${link.id}` }}>
+                      Voir le détail
+                    </Button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
