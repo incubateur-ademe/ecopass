bc0e2830fb8c398ef597191032ea90c7ef0166d3
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-06-05 13:13:58 +0200

    feat: add help buttons (#161)

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -11,11 +11,27 @@ const Login = () => {
         <div className='fr-grid-row fr-grid-row-gutters fr-grid-row--center'>
           <div className='fr-col-12 fr-col-md-9 fr-col-lg-8'>
             <h1>Connexion</h1>
+            {!test && (
+              <p>
+                La connexion au portail de déclaration de l’affichage environnemental est actuellement réservée aux
+                professionnels.
+              </p>
+            )}
+
             <p className='fr-mb-2w'>
-              {!test &&
-                "La connexion au portail de déclaration de l’affichage environnemental est actuellement réservée aux professionnels. "}
-              Si vous rencontrez des difficultés à vous connecter, veuillez contacter l’équipe de l’affichage
-              environnemental à cette adresse{" "}
+              Si vous rencontrez des difficultés à vous connecter,{" "}
+              <Link
+                href={
+                  test
+                    ? "https://docs.numerique.gouv.fr/docs/fd1182f0-2180-4a62-9531-bf23e812886e/"
+                    : "https://docs.numerique.gouv.fr/docs/9a00ac67-d9b4-44a1-8976-008da71a608b/"
+                }
+                target='_blank'
+                rel='noopener noreferrer'
+                className='fr-link'>
+                consulter les tutoriels de connexion
+              </Link>{" "}
+              ou contacter l’équipe de l’affichage environnemental à cette adresse{" "}
               <Link href={`mailto:${process.env.NEXT_PUBLIC_SUPPORT_MAIL}`} className='fr-link' prefetch={false}>
                 {process.env.NEXT_PUBLIC_SUPPORT_MAIL}
               </Link>
