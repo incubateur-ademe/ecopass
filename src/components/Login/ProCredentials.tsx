020fb2353735eea4898aee60cdfb48921a2088a1
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-08-21 11:14:04 +0200

    feat: add inscription on public login (#196)

---
+++ src/components/Login/ProCredentials.tsx
@@ -0,0 +1,156 @@
+"use client"
+
+import { Tabs } from "@codegouvfr/react-dsfr/Tabs"
+import styles from "./LoginForm.module.css"
+import { Button } from "@codegouvfr/react-dsfr/Button"
+import { track } from "../../utils/matomo"
+import classNames from "classnames"
+import { useRouter } from "next/navigation"
+import { FormEvent, useCallback, useState } from "react"
+import { signIn } from "next-auth/react"
+import { Input } from "@codegouvfr/react-dsfr/Input"
+import Link from "next/link"
+import { Alert } from "@codegouvfr/react-dsfr/Alert"
+import { messages } from "./messages"
+
+const ProCredentials = ({ as }: { as?: "h2" }) => {
+  const router = useRouter()
+  const [error, setError] = useState("")
+
+  const Title = as ? as : "h3"
+
+  const submit = useCallback(
+    async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      const formData = new FormData(event.currentTarget)
+      const email = formData.get("email")
+      const password = formData.get("password")
+      if (email && password) {
+        setError("")
+        track("Login", "Credentials", "Credentials")
+        const result = await signIn("credentials", {
+          email,
+          password,
+          redirect: false,
+        })
+
+        if (!result?.error) {
+          track("Login", "Credentials", "Success")
+          router.refresh()
+        } else {
+          if (result.error === "proconnect") {
+            track("Login", "Credentials", "Error ProConnect")
+          } else if (result.error === "franceconnect") {
+            track("Login", "Credentials", "Error FranceConnect")
+          } else {
+            track("Login", "Credentials", "Error")
+          }
+          setError(result.error)
+        }
+      }
+    },
+    [router],
+  )
+  return (
+    <>
+      <Title className={styles.title}>Je n’ai pas d’accès ProConnect</Title>
+      <Tabs
+        className='fr-mt-4w'
+        tabs={[
+          {
+            label: "Créer un accès",
+            isDefault: true,
+            content: (
+              <>
+                <p className={styles.description}>
+                  Vous n’avez pas de SIRET ou/et pas de compte France connect, remplissez ce formulaire pour fournir les
+                  pièces nécessaires à la création de votre espace.
+                </p>
+                <Button
+                  className={classNames("fr-mt-4w", styles.button)}
+                  linkProps={{
+                    href: "https://demarche.numerique.gouv.fr/commencer/registration-of-companies-without-a-siret-number-o",
+                    onClick: () => track("Login", "Credentials", "Create account"),
+                  }}>
+                  Je crée mon compte
+                </Button>
+              </>
+            ),
+          },
+          {
+            label: "Connexion",
+            content: (
+              <>
+                <form id='login-1760' onSubmit={submit}>
+                  <fieldset
+                    className='fr-fieldset'
+                    id='login-1760-fieldset'
+                    aria-labelledby='login-1760-fieldset-legend login-1760-fieldset-messages'>
+                    <legend id='login-1760-fieldset-legend'>
+                      <p className={styles.description}>
+                        Vous n'avez pas d'accès ProConnect, vous avez fait une demande d’accès, utilisez l'accès qui
+                        vous a été créé. Tous les champs sont obligatoires.
+                      </p>
+                    </legend>
+                    <div className='fr-fieldset__element'>
+                      <fieldset className='fr-fieldset'>
+                        <div className='fr-fieldset__element'>
+                          <Input
+                            className={styles.input}
+                            label='Email'
+                            hintText='Format attendu : nom@domaine.fr'
+                            nativeInputProps={{
+                              required: true,
+                              type: "email",
+                              name: "email",
+                            }}
+                          />
+                        </div>
+                        <div className='fr-fieldset__element'>
+                          <Input
+                            className={styles.input}
+                            label='Mot de passe'
+                            nativeInputProps={{
+                              required: true,
+                              type: "password",
+                              name: "password",
+                            }}
+                          />
+                        </div>
+                        <div className='fr-fieldset__element'>
+                          <p>
+                            <Link
+                              href='/forget-password'
+                              className={classNames("fr-link", styles.link)}
+                              prefetch={false}>
+                              Mot de passe oublié ?
+                            </Link>
+                          </p>
+                        </div>
+                      </fieldset>
+                    </div>
+                    <Button className={styles.button} type='submit'>
+                      Se connecter
+                    </Button>
+                  </fieldset>
+                </form>
+                {error && (
+                  <Alert
+                    className='fr-mt-4w'
+                    title={
+                      messages[error] ||
+                      "Une erreur est survenue lors de la connexion. Veuillez vérifier vos identifiants et réessayer."
+                    }
+                    severity='error'
+                  />
+                )}
+              </>
+            ),
+          },
+        ]}
+      />
+    </>
+  )
+}
+
+export default ProCredentials
--- src/components/Login/PublicLoginForm.tsx
+++ src/components/Login/PublicLoginForm.tsx
@@ -5,12 +5,14 @@ import { signIn } from "next-auth/react"
 import styles from "./LoginForm.module.css"
 import { ProConnectButton } from "@codegouvfr/react-dsfr/ProConnectButton"
 import { FranceConnectButton } from "@codegouvfr/react-dsfr/FranceConnectButton"
+import ProCredentials from "./ProCredentials"
+import CitoyenCredentials from "./CitoyenCredentials"
 
-const PublicLoginForm = ({ callbackUrl }: { callbackUrl: string }) => {
+const PublicLoginForm = ({ test, callbackUrl }: { test?: boolean; callbackUrl: string }) => {
   return (
     <>
       <div className={styles.box}>
-        <h3>Vous souhaitez déclarer en tant que citoyen ?</h3>
+        <h2>Vous souhaitez déclarer en tant que citoyen ?</h2>
         <p className={styles.description}>
           Via l’outil France Connect. Votre identité reste connue de la plateforme mais sera non visible du grand
           public, vous déclarez en tant que “citoyen”.
@@ -21,9 +23,14 @@ const PublicLoginForm = ({ callbackUrl }: { callbackUrl: string }) => {
             signIn("franceconnect", { callbackUrl })
           }}
         />
+        {!test && (
+          <div className='fr-mt-8w'>
+            <CitoyenCredentials />
+          </div>
+        )}
       </div>
       <div className={styles.box}>
-        <h3>Vous souhaitez déclarer en tant que professionnel ?</h3>
+        <h2>Vous souhaitez déclarer en tant que professionnel ?</h2>
         <p className={styles.description}>
           Je suis une entreprise française avec un numéro SIRET et un accès France connect (pour les entreprises de
           moins de 50 salariés)
@@ -34,6 +41,11 @@ const PublicLoginForm = ({ callbackUrl }: { callbackUrl: string }) => {
             signIn("proconnect", { callbackUrl })
           }}
         />
+        {!test && (
+          <div className='fr-mt-8w'>
+            <ProCredentials />
+          </div>
+        )}
       </div>
     </>
   )
