0ed8e34255fea9a38d605b41042762a80313d5d2
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-09-02 17:46:20 +0200

    feat: manage UPC codes

--- src/services/validation/gtins.ts
+++ src/services/validation/gtins.ts
@@ -5,7 +5,7 @@ export const gtinsValidation = z
   .array(
     z
       .string()
-      .regex(/^\d{8}$|^\d{13}$/, "Le code GTIN doit contenir 8 ou 13 chiffres")
+      .regex(/^\d{8}$|^\d{12}$|^\d{13}$/, "Le code GTIN doit contenir 8, 12 ou 13 chiffres")
       .refine(isValidGtin, "Le code GTIN n'est pas valide (somme de contrôle incorrecte)"),
     {
       message: "Il doit y avoir au moins un GTIN",
