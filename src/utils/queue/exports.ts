14ffdb1a0d949c1fd4d900f4c38f3419211bd067
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-09-04 10:18:05 +0200

    feat: split export in batches (#208)

--- src/utils/queue/exports.ts
+++ src/utils/queue/exports.ts
@@ -1,8 +1,9 @@
 import { completeExport, failExport, getFirstExport } from "../../db/export"
-import { getProductsByOrganizationIdAndBrandBefore, ProductWithScore } from "../../db/product"
+import { getOrganizationAuthorizedBrands, getProducts, ProductWithScore } from "../../db/product"
 import JSZip from "jszip"
 import { getSVG } from "../label/simple"
 import { uploadFileToS3 } from "../s3/bucket"
+import { Status } from "@prisma/enums"
 
 const renderLabelSVG = (product: ProductWithScore) => {
   if (!product.score || !product.standardized) {
@@ -12,41 +13,66 @@ const renderLabelSVG = (product: ProductWithScore) => {
   return getSVG(product.score, product.standardized)
 }
 
+const size = 1000
+
 export const processExportsQueue = async () => {
   const exportToProcess = await getFirstExport()
   if (!exportToProcess) {
     return
   }
   if (!exportToProcess.user.organizationId) {
-    completeExport(exportToProcess.id)
+    await failExport(exportToProcess.id)
     return
   }
 
   console.log(`Processing export ${exportToProcess.name}`)
-  const products = await getProductsByOrganizationIdAndBrandBefore(
-    exportToProcess.user.organizationId,
-    exportToProcess.createdAt,
-    exportToProcess.brand,
-  )
+  const authorizedBrands = await getOrganizationAuthorizedBrands(exportToProcess.user.organizationId)
 
-  if (!products.length) {
-    failExport(exportToProcess.id)
+  if (exportToProcess.brand && !authorizedBrands.has(exportToProcess.brand)) {
+    await failExport(exportToProcess.id)
     return
   }
 
-  const zip = new JSZip()
+  let products: ProductWithScore[] = []
+  let page = 0
 
-  for (const product of products) {
-    const svgContent = renderLabelSVG(product)
-    if (!svgContent) {
+  while (page === 0 || products.length > 0) {
+    products = await getProducts(
+      {
+        brandId: exportToProcess.brand ? exportToProcess.brand : { in: Array.from(authorizedBrands) },
+        status: Status.Done,
+        createdAt: { lt: exportToProcess.createdAt },
+      },
+      size * page,
+      size,
+    )
+
+    if (!products.length) {
+      if (page === 0) {
+        await failExport(exportToProcess.id)
+        return
+      }
+
       continue
     }
 
-    zip.file(`${product.internalReference}.svg`, svgContent)
+    console.log(`exporting page ${page} : ${products.length} products`)
+    const zip = new JSZip()
+
+    for (const product of products) {
+      const svgContent = renderLabelSVG(product)
+      if (!svgContent) {
+        continue
+      }
+
+      zip.file(`${product.internalReference}.svg`, svgContent)
+    }
+
+    const zipContent = await zip.generateAsync({ type: "nodebuffer" })
+    await uploadFileToS3(`${exportToProcess.name}${page > 0 ? `-${page + 1}` : ""}.zip`, zipContent, "export")
+    page++
   }
 
-  const zipContent = await zip.generateAsync({ type: "nodebuffer" })
-  await uploadFileToS3(`${exportToProcess.name}.zip`, zipContent, "export")
-
-  await completeExport(exportToProcess.id)
+  await completeExport(exportToProcess.id, page)
+  console.log(`Completed export ${exportToProcess.name}`)
 }
