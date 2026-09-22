8c93672d4533d162ad95f85c266e213b0e17baca
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-09-22 11:35:00 +0200

    chore: update logout timeout

--- e2e/utils/login.ts
+++ e2e/utils/login.ts
@@ -65,6 +65,6 @@ export const loginWithPassword = async (
 export const logout = async (page: Page) => {
   await page.getByRole("link", { name: "Se déconnecter" }).first().click()
 
-  await expect(page.getByRole("link", { name: "Se connecter", exact: true }).first()).toBeVisible()
+  await expect(page.getByRole("link", { name: "Se connecter", exact: true }).first()).toBeVisible({ timeout: 30000 })
   await expect(page.locator("#contenu").getByRole("heading", { name: "Affichage environnemental" })).toBeVisible()
 }
